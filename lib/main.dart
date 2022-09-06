ea0af99b4399af17de2696e6faae6a3a637e1a65
Abhishek Shrestha <absk.shrestha@gmail.com>
2022-09-06 23:40:39 +0545

    add font family google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../screens/groceries_overview_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'My Grcoceries',
+      themeMode: ThemeMode.dark,
+      
+      primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+    
+      ),
       home: GroceriesOverviewScreen(),
+      
     );
   }
 }
